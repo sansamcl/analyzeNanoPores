ea4e8d0a54fa71d8ac098acc91a8d837631abbf4
Chris Sansam <63733772+sansamcl@users.noreply.github.com>
2023-02-14 14:37:23 -0600

    Add files via upload

---
+++ README.md
@@ -0,0 +1,15 @@
+```
+module purge
+module load slurm seqtk/1.2 blat/35
+
+ls -1 *.fastq | parallel 'seqtk seq -a {} > $(basename -s .fastq {}).fa'
+ls -1 *.fa | parallel 'blat {} target.fa $(basename -s .fa {}).psl'
+ls -1 *.psl | parallel 'tail -n +6 {} > $(basename -s .psl {})_blat_results.txt
+```
+
+```
+module purge
+module load slurm R bioconductor
+
+sbatch --wrap="Rscript --vanilla parseReads.R Sansam_d5s_1_3B1_1D2_Linear_991"
+```
---
+++ parseReads.R
@@ -0,0 +1,71 @@
+#!/usr/bin/env Rscript
+args = commandArgs(trailingOnly=TRUE)
+
+library(stringr)
+library("Biostrings")
+
+sample_name <- args[1]
+
+blat_results <- read.table(paste0(args[1],"_blat_results.txt"))
+fastas <- readDNAStringSet(paste0(args[1],".fa"))
+
+crisprSite <- "GACGCTGCTAGACTACCAGT" %>% DNAStringSet %>% {names(.) <- "crispr";.}
+pcrFull_2490 <- "CAGTGAGTGGTGCTGTTTCCctgaaggaagggactaagggacggtggcgcgggcccggaccggggccccggggcggcggcacggccgatatggcatgctgtcacaaagtaatgctgctgctggacaccgcgggcggcgccgcccgccacagccgggtccggcgggccgccctgcgcctcctcacctatctgagttgccgattcggcctggccagggtccactgggccttcaagttctttgactcgcagggggcgcggagccggccgtcccgcgtgtctgacttccgcgagctggggtcccgctcgtgggaggactttgaggaggagctggaggccaggctcgaggatcgcgcccacctgcccggcccggcgcccagggccacccacacgcacggcgccctgatggagacgctgctagactaccagtgggaccggcccgagatcacgtcgcccacgaagccgatcctgcggagcagcgggaggagactgctggacgtggagagcgaggccaaggaggccgaggccgcgctcgggggcttggtgaacgccgtcttcctcctggccccctgtccgcactcgcagagggagctgctgcagttcgtgtctgggtgcgaggcccaggcccagcgcctgccgcccacccctaagcaggtgatggagaagttgttgcccaagagagtccgggaagtcatggtcgcccgaaaaatcaccttctactgggtggataccaccgaatggtctaaggtaaggaaggttactgtcgtctcagatggcgtgcacggtgctttccttgctaaccaGAACTTGGCAGCGTCCTTAG" %>%
+  DNAStringSet %>% {names(.) <- "2490";.}
+pcrFull_2492 <- "TCTCTCGGACCACAGAGACCcgcccaccgtcactccctccaccaatcagactgaagttttctccactgcccctccaatcaccgccaaaaatggaagttgctctcgtaacttacgtaaggcaagcagccaataggagcaaaccttggaaactagctcccccaatagcagcagttcctggagagcgcgggagcctttcgaccagggtcccaaaggaaagcagtgagtggtgctgtttccctgaaggaagggactaagggacggtggcgcgggcccggaccggggccccggggcggcggcacggccgatatggcatgctgtcacaaagtaatgctgctgctggacaccgcgggcggcgccgcccgccacagccgggtccggcgggccgccctgcgcctcctcacctatctgagttgccgattcggcctggccagggtccactgggccttcaagttctttgactcgcagggggcgcggagccggccgtcccgcgtgtctgacttccgcgagctggggtcccgctcgtgggaggactttgaggaggagctggaggccaggctcgaggatcgcgcccacctgcccggcccggcgcccagggccacccacacgcacggcgccctgatggagacgctgctagactaccagtgggaccggcccgagatcacgtcgcccacgaagccgatcctgcggagcagcgggaggagactgctggacgtggagagcgaggccaaggaggccgaggccgcgctcgggggcttggtgaacgccgtcttcctcctggccccctgtccgcactcgcagagggagctgctgcagttcgtgtctgggtgcgaggcccaggcccagcgcctgccgcccacccctaagcaggtgatggagaagttgttgcccaagagagtccgggaagtcatggtcgcccgaaaaatcaccttctactgggtggataccaccgaatggtctaaggtaaggaaggtTACTGTCGTCTCAGATGGCG" %>%
+  DNAStringSet %>% {names(.) <- "2492";.}
+crisprSite <- c(crisprSite,pcrFull_2490,pcrFull_2492)
+
+
+df <- lapply(1:nrow(blat_results),function(i){
+  blat_results_1 <- blat_results[i,]
+  block_sizes <- blat_results_1$V19 %>% str_split(.,",") %>% unlist %>% as.numeric %>% .[!is.na(.)]
+  block_starts <- blat_results_1$V20 %>% str_split(.,",") %>% unlist %>% as.numeric %>% .[!is.na(.)]
+  block_ends <- block_starts + block_sizes
+  ranges=paste(block_starts,block_ends,sep="_")
+  #gaps=paste(block_ends[-length(block_ends)],block_starts[-1],sep="_")
+  data.frame(blat_results_1$V14,block_starts,block_ends,ranges)
+}) %>% do.call("rbind",.)
+
+completeCRISPRSite <- df %>%
+  .[which(.$block_starts < 43),] %>%
+  .[which(.$block_ends > 66),] %>%
+  .$blat_results_1.V14
+
+df2 <- lapply(1:nrow(blat_results),function(i){
+  blat_results_1 <- blat_results[i,]
+  block_sizes <- blat_results_1$V19 %>% str_split(.,",") %>% unlist %>% as.numeric %>% .[!is.na(.)]
+  block_starts <- blat_results_1$V20 %>% str_split(.,",") %>% unlist %>% as.numeric %>% .[!is.na(.)]
+  block_ends <- block_starts + block_sizes
+  ranges=paste(block_starts,block_ends,sep="_")
+  gaps=paste(block_ends[-length(block_ends)],block_starts[-1],sep="_")
+  if(length(gaps) > 0){
+    data.frame(blat_results_1$V14,gaps)
+  } else {
+  }
+}) %>% do.call("rbind",.)
+
+block_starts <- table(df$block_starts) %>% .[which(.>1)] %>% .[rev(order(.))]
+block_ends <- table(df$block_ends) %>% .[which(.>1)] %>% .[rev(order(.))]
+ranges <- table(df$ranges) %>% .[which(.>1)] %>% .[rev(order(.))]
+
+gaps <- table(df2$gaps) %>% .[which(.>1)] %>% .[rev(order(.))]
+
+gaps2 <- paste(gaps,names(gaps), sep="_")
+
+directory_name <- paste(sample_name,"gap_fastas",sep="_")
+dir.create(directory_name, showWarnings = FALSE)
+setwd(directory_name)
+
+for (i in 1:length(gaps)){
+  gap <- gaps[i] %>% names %>% as.character
+  gap2 <- gaps2[i]
+  samples <- df2$blat_results_1.V14[which(df2$gaps == gap)]
+  fastas[samples] %>%
+    c(crisprSite,.) %>%
+    writeXStringSet(., paste0(gap2,".fasta"))
+}
+
+fastas[completeCRISPRSite] %>%
+  c(crisprSite,.) %>%
+  writeXStringSet(., "wt.fasta")
